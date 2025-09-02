a639293a0648052be99b60e20d744c69b4592149
ren <ren84690@gmail.com>
2025-09-02 13:12:01 +0900

    デプロイチェックver2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import Greeting from "./components/Greeting";
 import TaskList from "./components/TaskList";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h2 className="text-2xl font-bold mb-4">Frontend</h2>
       <Greeting />
       <TaskList />
+      <Button />
     </div>
   );
 }
