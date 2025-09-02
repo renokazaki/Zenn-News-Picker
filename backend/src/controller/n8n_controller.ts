73e6205fa58d83901d612b0dbf1f86a9fd7da521
ren <ren84690@gmail.com>
2025-09-02 15:21:18 +0900

    create n8n api and add news table

--- backend/src/controller/n8n_controller.ts
+++ backend/src/controller/n8n_controller.ts
@@ -8,9 +8,13 @@ const getHello = (req: Request, res: Response) => {
 };
 
 const postNews = async (req: Request, res: Response) => {
-  const { text } = req.body as { text: string };
+  const { text, title, url } = req.body as {
+    text: string;
+    title: string;
+    url: string;
+  };
   const news = await prisma.news.create({
-    data: { text },
+    data: { text, title, url },
   });
   res.status(201).json(news);
 };
