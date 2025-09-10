bf5214a54c6d8c0feca00dcc12b1466f873ffb8d
ren <ren84690@gmail.com>
2025-09-10 15:28:53 +0900

    backendのリファクタリング

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import routeApi from "./routers/routerApi";
-import route_n8n from "./routers/route_n8n";
+import route_n8n from "./n8n/route_n8n";
 
 const router = express.Router();
 
-router.use("/api", routeApi);
 router.use("/n8n", route_n8n);
 
 export default router;
