423c8663f4b523688200f561f2a8e697cae0c467
ren <ren84690@gmail.com>
2025-09-11 13:53:08 +0900

    feature/Jest-React Testing Libraryの導入

---
+++ __test__/sum.test.ts
@@ -0,0 +1,23 @@
+// import { sum } from "../src/sum";
+
+// // it("sum関数のテスト", () => {
+// //   expect(sum(1, 2)).toBe(3);
+// // });
+// // it("sum関数のテスト", () => {
+// //   expect(sum(1, 2)).toBe(3);
+// // });
+
+// it.each([
+//   [1, 2, 3],
+//   [2, 3, 5],
+// ])("sum(%i, %i)", (a, b, expected) => {
+//   expect(sum(a, b)).toBe(expected);
+// });
+
+// it.each`
+//   a    | b    | expected
+//   ${1} | ${2} | ${3}
+//   ${2} | ${3} | ${5}
+// `("sum($a, $b)", ({ a, b, expected }) => {
+//   expect(sum(a, b)).toBe(expected);
+// });
